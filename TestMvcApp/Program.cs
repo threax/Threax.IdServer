3640bafa94d6642457d8e59f1f4a5ca7579d6728
Threax <andrew@threax.com>
2017-07-28 19:45:58 -0400

    Removed app insights

--- TestMvcApp/Program.cs
+++ TestMvcApp/Program.cs
@@ -16,7 +16,6 @@ namespace TestMvcApp
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
