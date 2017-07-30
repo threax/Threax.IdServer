df4a138aaa48f15b3f68fc3600541b0ec5a45f6b
Threax <andrew@threax.com>
2017-07-30 16:34:50 -0400

    Got simple template working.

---
+++ TestApi/Migrations/AppDbContextModelSnapshot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TestApi.Database;
+
+namespace TestApi.Migrations
+{
+    [DbContext(typeof(AppDbContext))]
+    partial class AppDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TestApi.Database.ValueEntity", b =>
+                {
+                    b.Property<Guid>("ValueId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ValueId");
+
+                    b.ToTable("Values");
+                });
+        }
+    }
+}
--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Threax.AspNetCore.BuiltInTools;
 
 namespace TestApi
 {
@@ -12,14 +13,26 @@ namespace TestApi
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var tools = new ToolManager(args);
+            var env = tools.GetEnvironment();
+
+            var hostBuilder = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+                .UseStartup<Startup>();
 
-            host.Run();
+            if (env != null)
+            {
+                hostBuilder.UseEnvironment(env);
+            }
+
+            var host = hostBuilder.Build();
+
+            if (tools.ProcessTools(host))
+            {
+                host.Run();
+            }
         }
     }
 }
