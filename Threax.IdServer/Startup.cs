be8d51514c4f8b80a7c42504020b88e0e7bd8094
Andrew Piper <andrew@threax.com>
2017-12-24 10:33:49 -0500

    Changed to sqlite.

--- Threax.IdServer/Startup.cs
+++ Threax.IdServer/Startup.cs
@@ -29,7 +29,7 @@ namespace Threax.IdServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
