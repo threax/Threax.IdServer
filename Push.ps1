31004a41a4ad4f312107eef09e48c7f74f6b5b71
Andrew Piper <andrew@threax.com>
2021-01-19 14:45:16 -0500

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push
+git push https://github.com/threax/Threax.IdServer.git
+git push https://gitlab.com/threax-software/threax.idserver.git
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.IdServer
