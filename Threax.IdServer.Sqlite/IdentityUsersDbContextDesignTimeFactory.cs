dcfbfe188948511dc52d040641d9a13b1087e53a
Andrew Piper <andrew@threax.com>
2021-05-18 11:39:27 -0400

    Updated nuget packages.

---
+++ Threax.IdServer.Sqlite/IdentityUsersDbContextDesignTimeFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threax.IdServer.EntityFramework.DbContexts;
+
+namespace Threax.IdServer.Sqlite
+{
+    public class IdentityUsersDbContextDesignTimeFactory : IDesignTimeDbContextFactory<IdentityUsersDbContext>
+    {
+        public IdentityUsersDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<IdentityUsersDbContext>();
+            optionsBuilder.UseConnectedDb("ConnectionString");
+
+            return new IdentityUsersDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Threax.IdServer.Sqlite/OperationDbContextDesignTimeFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threax.IdServer.EntityFramework.DbContexts;
+
+namespace Threax.IdServer.Sqlite
+{
+    public class OperationDbContextDesignTimeFactory : IDesignTimeDbContextFactory<OperationDbContext>
+    {
+        public OperationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OperationDbContext>();
+            optionsBuilder.UseConnectedDb("ConnectionString");
+
+            return new OperationDbContext(optionsBuilder.Options);
+        }
+    }
+}
